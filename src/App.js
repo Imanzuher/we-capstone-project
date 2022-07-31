51a1de5acb433e1d02eb45654f85a02bdb1908dd
Imanzuher <imoojaf@gmail.com>
2022-08-01 01:31:18 +0300

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Categories from "./pages/Categories";
@@ -9,16 +10,19 @@ import Cart from "./pages/Cart";
 import Register from "./pages/Register";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="categories" element={<Categories />} />
-      <Route path="products" element={<Products />} />
-      <Route path="product" element={<Product />} />
-      <Route path="login" element={<Login />} />
-      <Route path="shoppingCart" element={<Cart />} />
-      <Route path="register" element={<Register />} />
-    </Routes>
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="products" element={<Products />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
+        <Route path="shoppingCart" element={<Cart />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </div>
   );
 }
 
