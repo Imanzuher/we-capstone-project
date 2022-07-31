3234cd66b4aed0f0ed1b7ef3d16c277ded369aff
Imanzuher <imoojaf@gmail.com>
2022-08-01 00:52:46 +0300

    add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Categories from "./pages/Categories";
+import Products from "./pages/Products";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Register from "./pages/Register";
 function App() {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline bg-orange-500">Hello world!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="categories" element={<Categories />} />
+      <Route path="products" element={<Products />} />
+      <Route path="product" element={<Product />} />
+      <Route path="login" element={<Login />} />
+      <Route path="shoppingCart" element={<Cart />} />
+      <Route path="register" element={<Register />} />
+    </Routes>
   );
 }
 
