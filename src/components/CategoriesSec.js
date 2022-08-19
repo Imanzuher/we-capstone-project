f04e39b5301e1260ad4af7fe292fe00ca827e119
Imanzuher <imoojaf@gmail.com>
2022-08-19 22:03:12 +0300

    change categories style

--- src/components/CategoriesSec.js
+++ src/components/CategoriesSec.js
@@ -11,39 +11,41 @@ function CategoriesSec() {
         <h1>Our Categories</h1>
       </div>
       <div className="flex mt-10 mb-72 justify-around items-center text-center mx-52 ">
-        <div className="border py-6 px-8 border-gray-700 cursor-pointer  rounded-full transition ease-in-out hover:border-dashed hover:border-2 hover:border-amber-500 duration-500n-200  ">
-          <div className="w-20 opacity-80 ">
+        <div className="border py-6 px-8 border-gray-700 cursor-pointer  rounded-full transition-all hover:border-dashed hover:border-2 hover:border-amber-500 duration-500n-200  ">
+          <div className="w-16 opacity-80 ">
             <img src={bedroom} alt="" />
           </div>{" "}
-          <p className="font-semibold text-base text-gray-700">bedroom</p>
+          <p className="font-semibold text-sm text-gray-700 mt-2">bedroom</p>
         </div>
 
-        <div className="border py-6 px-8 border-gray-700 cursor-pointer  rounded-full transition ease-in-out hover:border-dashed hover:border-2 hover:border-amber-500 duration-500n-200 ">
+        <div className="border py-6 px-8 border-gray-700 cursor-pointer  rounded-full transition-all hover:border-dashed hover:border-2 hover:border-amber-500  ">
           {" "}
-          <div className="w-20 opacity-80">
+          <div className="w-16 opacity-80">
             <img src={sofa} alt="" />
           </div>
-          <p className="font-semibold text-base text-gray-700">living room</p>
+          <p className="font-semibold text-sm text-gray-700 mt-2">living </p>
         </div>
 
-        <div className="border py-6 px-8 border-gray-700 cursor-pointer rounded-full transition ease-in-out hover:border-dashed hover:border-2 hover:border-amber-500 duration-500n-200 ">
-          <div className="w-20 opacity-80">
+        <div className="border py-6 px-8 border-gray-700 cursor-pointer rounded-full transition-all hover:border-dashed hover:border-2 hover:border-amber-500 duration-500n-200 ">
+          <div className="w-16 opacity-80">
             <img src={table} alt="" />
           </div>
-          <p className="font-semibold text-base text-gray-700">dining room</p>
+          <p className="font-semibold text-sm text-gray-700 mt-2">dining </p>
         </div>
-        <div className="border py-6 px-8 border-gray-700 cursor-pointer rounded-full transition ease-in-out hover:border-dashed hover:border-2 hover:border-amber-500 duration-500n-200 ">
+        <div className="border py-6 px-8 border-gray-700 cursor-pointer rounded-full transition-all hover:border-dashed hover:border-2 hover:border-amber-500 duration-500n-200 ">
           {" "}
-          <div className="w-20 opacity-80">
+          <div className="w-16 opacity-80">
             <img src={lounge} alt="" />
           </div>
-          <p className="font-semibold text-base text-gray-700"> lounge</p>
+          <p className="font-semibold text-sm text-gray-700 mt-2"> lounge</p>
         </div>
-        <div className="border py-6 px-8 border-gray-700 cursor-pointer rounded-full transition ease-in-out hover:border-dashed hover:border-2 hover:border-amber-500 duration-500 ">
-          <div className="w-20 opacity-80">
+        <div className="border py-6 px-8 border-gray-700 cursor-pointer rounded-full transition-all hover:border-dashed hover:border-2 hover:border-amber-500 duration-500 ">
+          <div className="w-16 opacity-80">
             <img src={chair} alt="" />
           </div>
-          <p className="font-semibold text-base text-gray-700">office chair</p>
+          <p className="font-semibold text-sm text-gray-700 mt-2">
+            office chair
+          </p>
         </div>
       </div>
     </div>
